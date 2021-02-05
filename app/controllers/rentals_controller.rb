d07d2028c00d9a2f0f3c7ca2ef86d6aa5f81cae7
Jahaira Marrero <jahaira.marrero@gmail.com>
2021-02-04 19:25:49 -0500

    done

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,17 @@
 class RentalsController < ApplicationController
+
+    def new
+        @rental = Rental.new
+    end
+
+    def create
+        @rental = Rental.create(rental_params)
+        redirect_to "/artists"
+    end
+
+    private
+
+    def rental_params
+        params.require(:rental).permit(:artist_id, :instrument_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,15 @@ Rails.application.routes.draw do
   post "/artists", to: "artists#create"
   get "/artists/:id", to: "artists#show", as: "artist"
   get "/artists/:id/edit", to: "artists#edit", as: "edit_artist"
+  patch "/artists/:id", to: "artists#update"
+  delete "/artists/:id", to: "artists#destroy"
 
 
 
   #Rental
-
+  get "/rentals/new", to: "rentals#new", as: "new_rental"
+  post "/rentals", to: "rentals#create"
 
   #Instrument
+  get "/instruments", to: "instruments#index", as: "instruments"
 end
